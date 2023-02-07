f13b78d811c6047e7fb3ded9510469ab09564c91
Varun Shembekar <varun247shembekar@gmail.com>
2023-02-07 16:34:31 -0500

    Create helpers for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
-  
-  get "/" , to: "main#index"
+  root to: "main#index"
 end
