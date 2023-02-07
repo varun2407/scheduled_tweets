49ac3112eef153331202ee2f69f362e4c96a5b20
Varun Shembekar <varun247shembekar@gmail.com>
2023-02-07 14:37:28 -0500

    Create Routes and Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "about", to: "about#index"
+  
+  get "/" , to: "main#index"
 end
