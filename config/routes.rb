8bf40d9ba4a7afda6f6d2e4334d5d64946322398
Varun Shembekar <varun247shembekar@gmail.com>
2023-02-08 15:18:46 -0500

    Add Login page and edit password

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
 
   root to: "main#index"
+
   get "sign_up", to: "registrations#new"
-  post "users", to: "registrations#create"
+  post"sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post"sign_in", to: "sessions#create"
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update" 
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
+  delete"logout", to: "sessions#destroy"
 end
